ab9bc5210209429de4feb6992a8bd9424708c378
Dulan Samarasingha <dulansamarasingha138@gmail.com>
2025-02-25 17:02:01 +0530

    fronend-figures

--- frontend/dms/src/App.jsx
+++ frontend/dms/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Feagures from './components/Feagures/Feagures'
 
 
 const App = () => {
@@ -10,6 +11,10 @@ const App = () => {
     <div>
       <Navbar/>
       <Hero/>
+      <div className="container">
+          <Feagures/>
+      </div>
+      
     </div>
 
   )
