9020ef53b258a814aaf1d14c059caeda5902592f
Dulan Samarasingha <dulansamarasingha138@gmail.com>
2025-02-25 14:28:30 +0530

    frontend-nav-bar

---
+++ frontend/dms/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/Navbar-logo.png'
+
+const Navbar = () => {
+
+  return (
+
+    <nav className='container'>
+
+      <div className='logo-div'>
+        <img src= {logo} alt='logo' className='logo' />
+        <p className='logo-text'>SAHANAYA</p>
+      </div>
+      
+      <ul>
+        <li>Home</li>
+        <li>Dashboard</li>
+        <li>Patients</li>
+        <li>Drugs</li>
+        <li>Reports</li>
+        <li>About Us</li>
+        <li><button className='btn'>Contact Us</button></li>
+      </ul>
+
+
+    </nav>
+  )
+}
+
+export default Navbar
